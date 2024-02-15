6fe63848bbaea3498ac64d87cc86accf893ac286
Jesus0204 <a01705442@itesm.mx>
2024-02-15 10:04:48 -0600

    Lab 4: Comenzado

---
+++ Lab 4: Fundamentos Javascript/intro_js.js
@@ -0,0 +1,15 @@
+// Con esto se imprime 
+console.log("hola");
+
+// Manda informacion
+console.info("Esto es información");
+
+// Manda una advertencia
+console.warn("Esto es una advertencia");
+
+// Manda un error
+console.error("Esto es un error");
+
+// Es para hacer pruebas. Si es verdadero no hace nada, y si si manda un error
+console.assert(1==1);
+console.assert(1==2);
