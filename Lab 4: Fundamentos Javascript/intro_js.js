cf4907ffaa367944f31678a51ed47eb9ed6f81d1
Jesus0204 <a01705442@itesm.mx>
2024-02-15 10:11:03 -0600

    Lab 4: Variables y constantes

--- Lab 4: Fundamentos Javascript/intro_js.js
+++ Lab 4: Fundamentos Javascript/intro_js.js
@@ -13,3 +13,14 @@ console.error("Esto es un error");
 // Es para hacer pruebas. Si es verdadero no hace nada, y si si manda un error
 console.assert(1==1);
 console.assert(1==2);
+
+// variables, constantes
+
+// Forma antigua de declarar variables
+var personaje = "Deadpool";
+
+// Constantes
+const precio = 50;
+
+// Forma moderna y segura de declarar variable
+let cantidad = 1;
