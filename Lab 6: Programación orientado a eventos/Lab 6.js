f5b74fb60ec26e25e748aaf5a63cfbb07fde5c6e
Jesus0204 <a01705442@tec.mx>
2024-02-19 14:52:25 -0600

    Lab 6: Validación de Passwords

--- Lab 6: Programación orientado a eventos/Lab 6.js
+++ Lab 6: Programación orientado a eventos/Lab 6.js
@@ -5,7 +5,31 @@ const cancelar = () => {
     imagen.src = "https://hips.hearstapps.com/hmg-prod/images/deadpool-3-wolverine-65c95b3a552eb.jpg?resize=980:*";
 
     const title = document.getElementById("title");
-    title.innerHTML = "¿Porque lo cancelas? ¡Usa el validador!";
+    title.innerHTML = "¿Porque lo cancelas bro? ¡Usa el validador!";
+    title.className = "is-size-1 is-family-sans-serif";
 }
 
 cancel.onclick = cancelar;
+
+const validar = document.getElementById("boton_validar");
+
+const validar_pass = () => {
+    const password_1 = document.getElementById("password_1");
+    const password_2 = document.getElementById("password_2");
+
+    const password_1_value = password_1.value;
+    const password_2_value = password_2.value;
+
+    const paragraph = document.getElementById("paragraph");
+
+    if (password_1_value == password_2_value) {
+        paragraph.innerHTML = "¡La contraseña es correcta!";
+        paragraph.className = "has-text-success";
+    }
+    else {
+        paragraph.innerHTML = "¡Incorrecto! Por favor intenta de nuevo. :)";
+        paragraph.className = "has-text-danger-dark";
+    }
+}
+
+validar.onclick = validar_pass;
