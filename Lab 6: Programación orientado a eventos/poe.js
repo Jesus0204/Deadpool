44c8a2f61f2964fb4c32b6ef95e64a4a102a5ffb
Jesus0204 <a01705442@tec.mx>
2024-02-19 09:46:10 -0600

    Lab 6: Cambiar imagen con DOM

--- Lab 6: Programación orientado a eventos/poe.js
+++ Lab 6: Programación orientado a eventos/poe.js
@@ -1,5 +1,7 @@
 const boton = document.getElementById("boton_disparar");
 
 boton.onclick = () => {
-    console.log("click")
+    const imagen = document.getElementById("imagen_disparar");
+    imagen.src = "https://www.japantimes.co.jp/wp-content/uploads/2016/06/p9-hadfield-deadpool-a-20160602.jpg";
+    console.log(imagen);
 };
