4f98a61aa8408642c17853263379776ee8d120a2
Jesus0204 <a01705442@tec.mx>
2024-02-19 10:53:24 -0600

    Lab 6: HTML Password

--- Lab 6: Programación orientado a eventos/poe_clase.js
+++ Lab 6: Programación orientado a eventos/poe_clase.js
@@ -5,7 +5,7 @@ const disparar_enemigo = () => {
     imagen.src = "https://www.japantimes.co.jp/wp-content/uploads/2016/06/p9-hadfield-deadpool-a-20160602.jpg";
     console.log(imagen);
 
-    boton.innerHTML = "Matar enemigo";
+    boton.innerHTML = "Matar Enemigo";
     boton.className = "button is-danger is-light";
     boton.onclick = matar_enemigo;
 
@@ -17,7 +17,7 @@ const matar_enemigo = () => {
     const imagen = document.getElementById("imagen_disparar");
     imagen.src = "https://variety.com/wp-content/uploads/2020/08/deadpool.jpg?w=1000&h=563&crop=1&resize=1000%2C563";
 
-    boton.innerHTML = "Rematar enemigo";
+    boton.innerHTML = "Rematar Enemigo";
     boton.className = "button is-danger";
 
     boton.onclick = rematar_enemigo;
@@ -28,9 +28,19 @@ const rematar_enemigo = () => {
     const imagen = document.getElementById("imagen_disparar");
     imagen.src = "https://hips.hearstapps.com/digitalspyuk.cdnds.net/17/02/1484222978-deadpool.jpg?resize=1200:*";
 
-    boton.innerHTML = "Disparar enemigo";
+    boton.innerHTML = "Disparar Enemigo";
     boton.className = "button is-primary";
 
     boton.onclick = disparar_enemigo;
 }
+
+const casa = document.getElementById("boton_casa")
+
+const Esta_es_mi_casa = () => {
+    const imagen = document.getElementById("imagen_disparar");
+    imagen.src = "https://www.branding.news/wp-content/uploads/2018/05/deadpool-cover.jpg";
+    casa.innerHTML = "¿Qué haces en mi casa?";
+}
+
+casa.onclick = Esta_es_mi_casa;
 
