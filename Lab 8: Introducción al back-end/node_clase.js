6a1cfc6ebdc3b2614e24fd9015a9182828f41801
Jesus0204 <a01705442@tec.mx>
2024-02-22 10:09:55 -0600

    Lab 8: Ejemplo código asincronico

--- Lab 8: Introducción al back-end/node_clase.js
+++ Lab 8: Introducción al back-end/node_clase.js
@@ -5,3 +5,15 @@ console.log("hola mundo desde node");
 const filesystem = require('fs');
 // Escribe el string segundo parámetro en el archivo indicado en el primer paramétro
 filesystem.writeFileSync("hola.txt", "Hola desde node");
+
+setTimeout(() => {
+    console.log("jojo te hackié");
+}, 7000);
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+for (let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
