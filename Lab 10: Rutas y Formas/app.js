9dda52097198f6b60f88f759f0aedbd6d54a679c
Jesus0204 <a01705442@tec.mx>
2024-02-26 09:42:56 -0600

    Lab 10: 404 ruta

--- Lab 10: Rutas y Formas/app.js
+++ Lab 10: Rutas y Formas/app.js
@@ -79,10 +79,79 @@ const server = http.createServer( (request, response) => {
           `);
         // Envía la respuesta del servidor
         response.end();
-    } else if (request.url == "/") {
+    } else if (request.url == "/deadpool") {
         
     } else {
-
+        // Regresas el error 404 (código de respuesta para recurso no encontrado)
+        response.statusCode = 404;
+        // Regresas el html que dice que no existe
+        response.setHeader("Content-Type", "text/html");
+        response.write(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>¡¡Lab 10: Rutaaas!!</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+            <!-- Aquí se usan los íconos de font awesome  -->
+            <script src="https://kit.fontawesome.com/98026b1a47.js" crossorigin="anonymous"></script>
+          </head>
+          <body>
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                  <a class="navbar-item" href="https://bulma.io">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Deadpool_logo.jpg" width="112" height="28">
+                  </a>
+              
+                  <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                  </a>
+                </div>
+              
+                <div id="navbarBasicExample" class="navbar-menu">
+                  <div class="navbar-start">
+                    <a id="boton_casa" class="navbar-item">
+                      Esta es mi casa
+                    </a>
+              
+                    <a class="navbar-item">
+                      Y este es un nav item que no hace nada
+                    </a>
+                  </div>
+              
+                  <div class="navbar-end">
+                    <div class="navbar-item">
+                      <div class="buttons">
+                        <a id="boton_disparar" class="button is-primary">
+                          <strong>Disparar Enemigo</strong>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+          <section class="section">
+            <div class="container">
+              <h1 id="title" class="title">Oops, no existe el enemigo que estas buscando</h1>
+            </div>
+          </section>
+          <footer class="footer">
+            <div class="content has-text-centered">
+              <p>
+                <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+              </p>
+            </div>
+          </footer>
+          </body>
+          </html>
+          `);
+          // Envías la respuesta del servidor
+        response.end();
     }
 });
 
