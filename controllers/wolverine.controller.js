426ff8d6c674ec9e5c12bac7e444ee5391f1593a
Jesus0204 <a01705442@tec.mx>
2024-03-05 09:51:43 -0600

    Lab 14: Agregar primera cookie

--- controllers/wolverine.controller.js
+++ controllers/wolverine.controller.js
@@ -1,5 +1,5 @@
 exports.get_wolverine = (request, response, next) => {
-    response.render('mensaje_wolverine');
+  response.render('mensaje_wolverine');
 };
 
 const Wolverine_Message = require('../models/wolverine_message.model');
@@ -7,10 +7,12 @@ const Wolverine_Message = require('../models/wolverine_message.model');
 exports.post_wolverine = (request, response, next) => {
   console.log(request.body);
   // Creas una nueva instancia de la clase con su titulo y mensaje
-  const message = 
+  const message =
     new Wolverine_Message(request.body.titulo, request.body.mensaje);
   // Llamas al método de la clase que guarda eso en el arreglo
   message.save();
+  // Guardas una cookie mandandola en el header, guardando el último mensaje del usuario
+  response.setHeader('Set-Cookie', 'ultimo_mensaje=' + request.body.mensaje);
   response.redirect('/');
 };
 
