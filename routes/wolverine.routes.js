f4becd171f68e645f6bdcd4106b1917de99d4ace
Jesus0204 <a01705442@tec.mx>
2024-03-11 11:35:07 -0600

    Lab 18: Proteger las rutas con isAuth

--- routes/wolverine.routes.js
+++ routes/wolverine.routes.js
@@ -6,15 +6,18 @@ const router = express.Router();
 
 // Incluyes el modulo de tu controller en routes
 const wolverineController = require('../controllers/wolverine.controller');
+
+// Incluyes el archivo para verificar si esta autenticado
+const isAuth = require('../util/is-auth');
   
 // Le dices para esa ruta, que use el controlador con la acción get_wolverine
 // Le pasas el apuntador a la acción que tiene que ejecutar
-router.get('/wolverine', wolverineController.get_wolverine);
+router.get('/wolverine', isAuth, wolverineController.get_wolverine);
 
 // Haces lo mismo para las otras rutas, llamando al controlador con sus acciones
-router.post('/wolverine', wolverineController.post_wolverine);
-router.get('/:mensaje_id', wolverineController.get_mensajes);
-router.get('/', wolverineController.get_mensajes);
+router.post('/wolverine', isAuth, wolverineController.post_wolverine);
+router.get('/:mensaje_id', isAuth, wolverineController.get_mensajes);
+router.get('/', isAuth, wolverineController.get_mensajes);
 
 // Con esta linea se permite que se exporte en el principal
 module.exports = router;
