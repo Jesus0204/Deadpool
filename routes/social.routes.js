66a8e73551288e937c57a152134e05055059c2eb
Jesus0204 <a01705442@tec.mx>
2024-03-11 18:57:51 -0600

    Lab 18: Corregir rutas sin autenticacion

--- routes/social.routes.js
+++ routes/social.routes.js
@@ -10,12 +10,12 @@ const socialController = require('../controllers/social.controller');
 // Incluyes el archivo para verificar si esta autenticado
 const isAuth = require('../util/is-auth');
 
-router.get('/deadpool!', isAuth, socialController.get_deadpool);
+router.get('/deadpool!', socialController.get_deadpool);
 router.get('/instagram/crear_post', isAuth, socialController.get_crear_post);
 router.post('/instagram/crear_post', isAuth, socialController.post_crear_post);
 router.get('/instagram/:insta_id', isAuth, socialController.get_instagram);
 router.get('/instagram/', isAuth, socialController.get_instagram);
-router.get('/trailer', isAuth, socialController.get_trailer);
+router.get('/trailer', socialController.get_trailer);
 
 // Con esta linea se permite que se exporte en el principal
 module.exports = router;
